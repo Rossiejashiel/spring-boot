531caca339cf4d70b7a10db95897cc8111882c3c
Erick Gutierrez <erick.gugo@gmail.com>
2022-02-04 15:45:12 -0400

    Se corrigio filed de reporte, y tipo de export

--- demoweb2/src/main/java/com/exampleweb2/demoweb2/controller/DemoController.java
+++ demoweb2/src/main/java/com/exampleweb2/demoweb2/controller/DemoController.java
@@ -4,18 +4,25 @@ package com.exampleweb2.demoweb2.controller;
 
 
 import com.exampleweb2.demoweb2.model.Persona;
+import com.exampleweb2.demoweb2.service.ReportService;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.exampleweb2.demoweb2.repo.IPersonaRepo;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileNotFoundException;
 
 @Controller
 public class DemoController {
     @Autowired(required = true)
     private IPersonaRepo repo;
+    @Autowired
+    private ReportService service;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
@@ -33,4 +40,12 @@ public class DemoController {
         model.addAttribute("persona", repo.findAll());
         return "greeting";
     }
+
+    @GetMapping("/report/{format}")
+    public String generateReport(@PathVariable String format) throws FileNotFoundException, JRException {
+
+
+        return service.exportReport(format);
+    }
+
 }
--- demoweb2/src/main/java/com/exampleweb2/demoweb2/service/ReportService.java
+++ demoweb2/src/main/java/com/exampleweb2/demoweb2/service/ReportService.java
@@ -1,2 +1,39 @@
-package com.exampleweb2.demoweb2.service;public class ReportService {
+package com.exampleweb2.demoweb2.service;
+
+import com.exampleweb2.demoweb2.model.Persona;
+import com.exampleweb2.demoweb2.repo.IPersonaRepo;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ReportService {
+    @Autowired
+    private IPersonaRepo repository;
+
+    public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
+        List<Persona> persona = repository.findAll();
+        File file = ResourceUtils.getFile("classpath:persona.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(persona);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createBy", "Java Techie");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        if (reportFormat.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, "C:\\Users\\USER\\Desktop" + "\\employees.html");
+        }
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            JasperExportManager.exportReportToPdfFile(jasperPrint, "E:\\" + "employees.pdf");
+
+        }
+        return "reporte generado en el escritorio";
+    }
 }
